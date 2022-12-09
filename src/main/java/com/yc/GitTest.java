26ff3780e068f7b36d07650b87ad303adec58fb9
pangzi <1736364641@qq.com>
2022-12-09 16:48:35 +0800

    push commmit

--- src/main/java/com/yc/GitTest.java
+++ src/main/java/com/yc/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello,Git3");
         System.out.println("master,Git3");
         System.out.println("hot-fix,Git3");
+        System.out.println("push git-test");
     }
 }
